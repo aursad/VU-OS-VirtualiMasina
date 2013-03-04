7b01d3135ab2e3e64e5635da005151d2d1703a9b
unknown <aurimas.sadauskas@gmail.com>
2013-03-04 23:46:22 +0200

    VA update

--- src/OS/VA.java
+++ src/OS/VA.java
@@ -1,5 +1,7 @@
 package OS;
 
+import java.util.LinkedList;
+
 /**
  * Vartotojo atmintis
  * @author Aurimas
@@ -10,7 +12,7 @@ public class VA {
 	 * Vartotojui iðskiriamos atminties kiekis
 	 */
 	private int vartotojoAtmintis=0;
-	
+	LinkedList <String> atmintis;
 	
 	/**
 	 * Konstruktoriai
@@ -19,6 +21,22 @@ public class VA {
 		this(100);
 	}
 	VA(int vartotojoAtmintis) {
+		this.atmintis = new LinkedList<String>();
 		this.vartotojoAtmintis = vartotojoAtmintis;
+	}
+	/**
+	 * Pridedam elementa á Vartotojo atmintá
+	 * @param Record Norimas áraðas
+	 */
+	public void add(String Record) {
+		this.atmintis.add(Record);
+	}
+	/**
+	 * Norimu adresu gauname elementà ið vartotoji atminties
+	 * @param a Atminties adresas
+	 * @return
+	 */
+	public String get(int a) {
+		return this.atmintis.get(a);
 	}
 }
