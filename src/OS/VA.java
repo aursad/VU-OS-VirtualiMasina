ae12444c782aeb649ca3d29096a7cde37e08e718
unknown <aurimas.sadauskas@gmail.com>
2013-03-04 22:15:05 +0200

    VM pack

--- src/OS/VA.java
+++ src/OS/VA.java
@@ -6,5 +6,19 @@ package OS;
  *
  */
 public class VA {
-
+	/**
+	 * Vartotojui iðskiriamos atminties kiekis
+	 */
+	private int vartotojoAtmintis=0;
+	
+	
+	/**
+	 * Konstruktoriai
+	 */
+	public VA() {
+		this(100);
+	}
+	VA(int vartotojoAtmintis) {
+		this.vartotojoAtmintis = vartotojoAtmintis;
+	}
 }
